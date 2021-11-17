1c4cc26f96b9910369c704d96a04e96b399072c4
Tommy <tommymaher10@gmail.com>
2021-11-17 04:14:48 +0000

    Initial Commit

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,21 @@
+import axios from 'axios';
+
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+const Popup = () => {
+  const getCurrentGas = () => {
+    axios
+      .get(
+        'https://data-api.defipulse.com/api/v1/egs/api/ethgasAPI.json?api-key=c784b468e4ffe935d6332f19e3708c354fb97834b591b908248fa0c2d479'
+      )
+      .then((response) => {
+        console.log(response, 'Data:');
+      })
+      .catch((error) => {
+        console.log(error, 'Error:');
+      });
+  };
+};
+
+console.log(Popup);
--- src/pages/Popup/Popup.jsx
+++ src/pages/Popup/Popup.jsx
@@ -7,7 +7,7 @@ const Popup = () => {
     <div className="App">
       <header className="App-header">
         Tootz.eth! It'll toot when you've got gas.
-        <p className="gas-price-now">Current Gas: 123gwei</p>
+        <p className="gas-price-now">Current Gas: </p>
       </header>
     </div>
   );
